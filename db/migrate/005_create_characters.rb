c83f428810dac5913ce182bbd4a8a74b9458ba1f
Daniel Schoch <d.schoch90@gmail.com>
2018-10-05 03:45:13 +0000

    automatically backed up by learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,6 +1,6 @@
 class CreateCharacters < ActiveRecord::Migration
   def change
-    create_table :shows do |t|
+    create_table :characters do |t|
       #primary key of :id is created for us!
       t.string :name
     end
